e13da37ce7c99a430af8a2dad6d49c731bb0a4bc
Scott Crowder <scrowder@gmail.com>
2024-05-15 22:36:37 -0500

    Initial Commit

---
+++ PlayingCardWorkoutSwiftUI/PlayingCardWorkoutSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCardWorkoutSwiftUIApp.swift
+//  PlayingCardWorkoutSwiftUI
+//
+//  Created by SCOTT CROWDER on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayingCardWorkoutSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
